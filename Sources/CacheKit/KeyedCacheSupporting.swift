3d75e0fc3940fefffda5f8b471c46047456897c3
Shaun Hubbard <shaun@splife.com>
2018-04-17 00:03:13 -0400

    fixed some bugaboos from xcode

--- Sources/CacheKit/KeyedCacheSupporting.swift
+++ Sources/CacheKit/KeyedCacheSupporting.swift
@@ -7,13 +7,13 @@ public protocol KeyedCacheSupporting: Database {
     ///
     /// Returns an empty future that triggers on successful storage
     static func get<D>(_ type: D.Type, forKey key: String, on conn: Self.Connection) throws -> Future<D?>
-    where D: Decodable
+        where D: Decodable
 
     /// Sets the value to `entity` stored associated with the `key`
     ///
     /// Returns an empty future that triggers on successful storage
     static func set<E>(_ entity: E, forKey key: String, on conn: Self.Connection) throws -> Future<Void>
-    where E: Encodable
+        where E: Encodable
 
     /// Removes the value associated with the `key`
     static func remove(_ key: String, on conn: Self.Connection) throws -> Future<Void>
