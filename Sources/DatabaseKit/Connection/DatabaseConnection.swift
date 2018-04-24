1910ffde3c66a3ad9a496ed4ea189c037ba76626
tanner0101 <me@tanner.xyz>
2018-04-23 22:30:46 -0400

    reconform DatabaseConnection to DatabaseConnectable

--- Sources/DatabaseKit/Connection/DatabaseConnection.swift
+++ Sources/DatabaseKit/Connection/DatabaseConnection.swift
@@ -2,11 +2,31 @@
 ///
 /// Most of the database interaction work is done through static methods on `Database` that accept
 /// a connection. However, there are a few things like `isClosed` and `close()` that a connection must implement.
-public protocol DatabaseConnection: Worker, Extendable {
+public protocol DatabaseConnection: DatabaseConnectable, Extendable {
     /// If `true`, this connection has been closed and is no longer valid.
     /// This is used by `DatabaseConnectionPool` to prune inactive connections.
     var isClosed: Bool { get }
 
     /// Closes the `DatabaseConnection`.
     func close()
+}
+
+extension DatabaseConnection {
+    /// See `DatabaseConnectable`.
+    public func databaseConnection<Database>(to database: DatabaseIdentifier<Database>) -> Future<Database.Connection> {
+        let future: Future<Database.Connection>
+        if let conn = self as? Database.Connection {
+            future = eventLoop.newSucceededFuture(result: conn)
+        } else {
+            let error = DatabaseKitError(
+                identifier: "databaseConnectable",
+                reason: "Could not convert `\(Self.self)` to `\(Database.Connection.self)`.",
+                possibleCauses: [
+                    "You are using a database connection from a different database."
+                ]
+            )
+            future = eventLoop.newFailedFuture(error: error)
+        }
+        return future
+    }
 }
