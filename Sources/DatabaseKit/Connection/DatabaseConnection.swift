60ae82e00e4a9f6bfbe0812171bd7edd4707e43c
tanner0101 <me@tanner.xyz>
2018-05-30 21:59:03 -0400

    fluent gm: add associated database type to connection

--- Sources/DatabaseKit/Connection/DatabaseConnection.swift
+++ Sources/DatabaseKit/Connection/DatabaseConnection.swift
@@ -3,6 +3,10 @@
 /// Most of the database interaction work is done through static methods on `Database` that accept
 /// a connection. However, there are a few things like `isClosed` and `close()` that a connection must implement.
 public protocol DatabaseConnection: DatabaseConnectable, Extendable {
+    /// This connection's associated database type.
+    associatedtype Database: DatabaseKit.Database
+        where Database.Connection == Self
+    
     /// If `true`, this connection has been closed and is no longer valid.
     /// This is used by `DatabaseConnectionPool` to prune inactive connections.
     var isClosed: Bool { get }
